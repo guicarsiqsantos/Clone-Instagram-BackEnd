cdf6e1c997a904200515dfdd15891654de587dab
Guilherme C. Santos <guicarsiqsantos@gmail.com>
2022-03-26 22:37:03 -0300

    Primeira parte projeto

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://semana:semana@cluster0.mmnwy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+app.use(cors());
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
+app.use(require("./routes"));
+
+server.listen(3333, () => console.log("> Servidor Online PORT: 3333 <"));
